3b2c2e0f7d81a70627b39c42aea0072b0f3e8a07
kirikiritarutaru <kirikia@gmail.com>
2020-07-17 00:07:18 +0900

    update dotfilesLink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,11 +3,11 @@
  ln -sf ~/dotfiles/.vimrc ~/.vimrc
  ln -sf ~/dotfiles/.zshrc ~/.zshrc
  ln -sf ~/dotfiles/.Xresources ~/.Xresources
+ mkdir -p ~/.vim/rc
  sudo ln -sf ~/dotfiles/rc/dein.toml ~/.vim/rc/dein.toml
  sudo ln -sf ~/dotfiles/rc/dein_lazy.toml ~/.vim/rc/dein_lazy.toml
+ mkdir -p ~/.config/nvim/
  ln -sf ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
  ln -sf ~/dotfiles/nvim/dein.toml ~/.config/nvim/dein.toml
  ln -sf ~/dotfiles/nvim/dein_lazy.toml ~/.config/nvim/dein_lazy.toml
- ln -sf ~/dotfiles/i3/config ~/.config/i3/config
- ln -sf ~/dotfiles/i3status/config ~/.config/i3status/config
  ln -sf ~/dotfiles/regolith/i3/config ~/.config/regolith/i3/config
