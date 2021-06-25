d592532226f676ffd674cb3c79938417f298884f
taru <kirikia@gmail.com>
2021-06-26 08:22:31 +0900

    update dotfilesLink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 mkdir -p ~/.tmux
-mkdir -p ~/.config/regolith/i3
-sudo add-apt-repository ppa:regolith-linux/release
-sudo apt install -y gcc make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev regolith-desktop-standard vim neovim zsh tree fcitx-mozc tmux git ranger
+mkdir -p ~/.cache/dein
+sudo apt install -y gcc make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev vim neovim zsh tree fcitx-mozc tmux git ranger
 chsh -s $(which zsh)
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -16,5 +15,4 @@ mkdir -p ~/.config/nvim/
 ln -sf ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/nvim/dein.toml ~/.config/nvim/dein.toml
 ln -sf ~/dotfiles/nvim/dein_lazy.toml ~/.config/nvim/dein_lazy.toml
-ln -sf ~/dotfiles/regolith/i3/config ~/.config/regolith/i3/config
 source ~/.zshrc
